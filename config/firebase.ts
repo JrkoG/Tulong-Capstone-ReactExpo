40c6217f4fb861147203ecb76b7d667ddcfc6f7d
Rean Jericho Guinto <reanjericho@gmail.com>
2026-04-23 21:13:40 +0800

    Add variables to dotenv

--- config/firebase.ts
+++ config/firebase.ts
@@ -5,12 +5,12 @@ import { getFirestore } from 'firebase/firestore';
 
 // Your config object
 const firebaseConfig = {
-  apiKey: "AIzaSyDHVyYBupJ7G7-bneYuwXsibqfFA5IzLvA",
-  authDomain: "tulong-app-c7aaa.firebaseapp.com",
-  projectId: "tulong-app-c7aaa",
-  storageBucket: "tulong-app-c7aaa.firebasestorage.app",
-  messagingSenderId: "499151598398",
-  appId: "1:499151598398:web:49919f7287e5105272726f"
+  apiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.EXPO_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.EXPO_PUBLIC_FIREBASE_APP_ID
 };
 
 // Initialize App
